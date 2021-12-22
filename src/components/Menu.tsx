389210bb2de605f56201f0690da4f0582d4b2823
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2021-12-22 16:33:24 +0100

    menu dinamici

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -14,7 +14,10 @@ console.log('render: ' + opened);
 
 return <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
 <div className="container-fluid">
-  <div className={cn('navbar-brand',{'opened-brand': opened})}  onClick={toggle} >Corso React</div>
+  <div className={cn('navbar-brand',{'opened-brand': opened})}  
+  onMouseOver={()=> setOpened(true)}
+  onMouseOut={() => setOpened(false)}
+  onClick={toggle} >Corso React</div>
   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
